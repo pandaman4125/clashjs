47e3f2a6b54ad43cfb099382a0613a974401672e
Chase Groom <groom.chase@gmail.com>
2015-09-26 16:58:29 +1200

    modified src/Players.js

--- src/Players.js
+++ src/Players.js
@@ -1,5 +1,5 @@
 module.exports = {
-  javierbyte: require('./players/javierbyte.js'),
+  panda: require('./players/javierbyte.js'),
   jgroom33: require('./players/jgroom33.js'),
   merica: require('./players/merica.js'),
 };
