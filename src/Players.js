4a993f5bf416d90447ce0e656d2cf5b369141615
Ian <groom.ian03@gmail.com>
2015-09-28 17:37:52 +1300

    modified src/Players.js

--- src/Players.js
+++ src/Players.js
@@ -1,5 +1,8 @@
 module.exports = {
   panda: require('./players/panda.js'),
   jgroom33: require('./players/jgroom33.js'),
-  Spock: require('./players/Spock.js'),
+  Spock: require('./players/spock.js'),
+  Paymark: require ('./players/paymark.js'),
+  Paymark1: require ('./players/paymark1.js'),
+  Parmark2: require ('./players/paymark2.js'),
 };
